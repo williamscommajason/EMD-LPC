58f2c17312a518cbdb3d8a336d27aeffe7de45b1
Jason <williamscommajason@github.com>
2019-02-19 00:48:18 -0800

    added functios to pack and unpack residual

--- LPC.py
+++ LPC.py
@@ -4,6 +4,7 @@ import scipy
 import scipy.signal as sig
 from scipy.io import loadmat
 import math
+import struct
 
 class LPC:
     """
@@ -17,15 +18,19 @@ class LPC:
 
     logger = logging.getLogger(__name__)
 
-    def __init__(self, order, pred_coeff, frame_width):
+    def __init__(self, order, frame_width):
 
 
         self.aa = [1, -2, 1]
         self.gains = None
         self.h = frame_width
+        self.npts = None
         self.d = None
-        self.order = 2
+        self.order = order
+        self.amp = None
+        self.fits = None
         self.err = None
+        self.r_err = None
         self.a = None
 
     def __call__(self, signal):
@@ -53,6 +58,7 @@ class LPC:
             h = self.h
             w = 2*h
             npts = len(signal)
+            self.npts = npts
             nhops = math.floor(npts/h) 
         
             edges = [0 for i in range(int((w-h)/2))]
@@ -83,7 +89,7 @@ class LPC:
         self.a = a
         self.gains = g
         self.err = err        
- 
+  
         return a,g,err
            
     def lpc_synth(self,a,g,err):
@@ -107,8 +113,82 @@ class LPC:
 
         return d        
 
-        
+    def get_amp(self, err,frame):
+        self.amp = np.mean(err[::frame])
+        if self.amp < 0:
+            self.logger.debug("ERROR AMPLITUDE == NEGATIVE")
+        else:
+            self.logger.debug("ERROR AMPLITUDE == POSITIVE")
 
+        return(np.mean(err[1::frame]))
+            
+    def get_fits(self,err):
+        h = self.h
+        nhops = math.floor(self.npts/self.h)
+        fits = []
+        for i in range(nhops):
+            p = np.polyfit(np.array(list(range((i*h)+2,(i+1)*h))),err[(i*h)+2:(i+1)*h],1)
+            fits.append(p)
+        self.fits = [x for sublist in fits for x in sublist]
+        return self.fits
+
+    def get_gains(self):
+       return self.gains
+
+    def recon_err(self,amp,fits):
+         
+        recon_error = np.zeros(self.npts)
+        h = self.h
+        nhops = math.floor(self.npts/self.h)
+        for i in range(nhops):
+            recon_error[(i*h)+2:(i+1)*h] = np.polyval(np.array(fits[i]),np.array(list(range((i*h)+2,(i+1)*h))))
+            
+        recon_error[::self.h] = amp
+        recon_error[1::self.h] = -amp
+        self.r_err = recon_error
+
+        return recon_error
+
+    def pack_residual(self):
+        packed = []
+        packed.append(self.gains)
+        packed.append(self.fits)
+
+        f = open("packed_residual.bin", 'w+b')
+        f.write(struct.pack('@d',self.amp))
+        
+        for item in packed:
+            for x in item:
+               f.write(struct.pack('@d', x))
+        
+        
+        f.close()
+        return packed
+        
+    def unpack_residual(self):
+     
+        f = open("packed_residual.bin", 'rb')
+
+        amp = struct.unpack('@d',f.read(struct.calcsize('d')))[0]
+ 
+        gains = []
+        for i in range(4):
+            gains.append(struct.unpack('@d',f.read(struct.calcsize('d')))[0])
+        
+        fits = []
+        while True:
+        
+            try:
+                fits.append(struct.unpack('@d',f.read(struct.calcsize('d')))[0])
+
+            except struct.error:
+                fits = np.reshape(fits,(4,2))
+                fits = fits.tolist()                     
+                break
+
+        return amp, gains, fits   
+       
+                
 
 if __name__ == "__main__":
     data = loadmat('r10.mat')
@@ -117,8 +197,13 @@ if __name__ == "__main__":
     #ts = list(range(100))
     #a,g,err,h = lpc_fit(ts, 2, 350)
     #print(a)
-    residual = LPC(2, [1,-2,1],350)
-    a,g,err = residual.lpc_fit(ts)
-    residual.lpc_synth(a,g,err)
-    print()
+    lpc = LPC(2,350)
+    a,g,err = lpc.lpc_fit(ts)
+    lpc.lpc_synth(a,g,err)
+    print(lpc.get_amp(err,lpc.h))
+    amp,gains,fits = lpc.unpack_residual()
+    recon_err = lpc.recon_err(amp,fits)
+    #print((recon_err - lpc.err).tolist())
+    r_err = lpc.lpc_synth(lpc.a,gains,lpc.r_err)
+    print(ts-r_err)
     
