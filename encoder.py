20142512b71c27f0ba3b324b5df31624e311c0cb
Jason <williamscommajason@github.com>
2019-02-19 22:49:44 -0800

    fixed LPC coding so that you demand losslessness prior to rice encoding

--- encoder.py
+++ encoder.py
@@ -5,6 +5,7 @@ import rice_decode
 import argparse
 import numpy as np
 import math
+np.set_printoptions(threshold=np.nan)
 
 def encoder():
 
@@ -12,37 +13,34 @@ def encoder():
     parser.add_argument('filename')
     args = parser.parse_args()
     x = np.load(args.filename)[:1400]
+    emd = run_EMD_encoder(x)
+    r_err = run_LPC_encoder(emd.residual)
+    emd.make_lossless(r_err)
+    rice_encode.compress(emd.error)
     
-    emd = run_EMD(x)
-    run_LPC(emd.residual)
-    run_rice_encode(emd.error)
 
-def run_EMD(x):
+def run_EMD_encoder(x):
 
     emd = EMD()
     emd.emd(x,None)
-    emd.get_error()
-    print(emd.get_error())
-
     return emd
 
-def run_LPC(residual):
+def run_LPC_encoder(residual):
 
     lpc = LPC(2,350)
     lpc.lpc_fit(residual)
     lpc.get_fits(lpc.err)
     lpc.get_amp(lpc.err,lpc.h)
     lpc.pack_residual()
+    amp,gains,fits = lpc.unpack_residual()
+    lpc.recon_err(amp,fits)
+    r_err = lpc.lpc_synth(lpc.aaa,gains,lpc.r_err)
 
-def run_rice_encode(error):
-
-    mylist = [1,2,4,8,16,32,64,128,256,512]
-    std = math.sqrt(np.var(error))
-    rounded = mylist.index(min(mylist, key = lambda x:abs(x-std)))    
-    rice_encode.compress(error,rounded)
-
+    return r_err
     
-        
+       
+
+ 
 if __name__ == "__main__":
     encoder()
        
