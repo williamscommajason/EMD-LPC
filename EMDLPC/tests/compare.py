55b608d696c1e565dff40f2819dd4a59ac352433
Jason <williamscommajason@github.com>
2019-04-29 14:27:36 -0700

    compress all timestreams

--- EMDLPC/tests/compare.py
+++ EMDLPC/tests/compare.py
@@ -6,27 +6,32 @@ import EMD
 fname = 'pb2-testdata.g3'
 file = core.G3File(fname)
 
-for i in range(4):
+for i in range(3):
     frame = next(file)
 
 flac_fs = []
 emd_fs = []
+count = 0
 
-for i in range(3):
-    timestreams = frame['RawTimestreams_I'].items()[:1000]
-    for k,ts in timestreams:
+try:
+    for i in range(4):
+        timestreams = frame['RawTimestreams_I'].items()[:1000]
+        for k,ts in timestreams:
+            count += 1
         
-        ts.SetFLACCompression(9)
-        flac_fs.append(len(pickle.dumps(ts)))
-        print(len(pickle.dumps(ts)))
+            ts.SetFLACCompression(9)
+            flac_fs.append(len(pickle.dumps(ts)))
+        
 
-        emd = EMD.EMD()
-        nbytes, f = emd.save(np.asarray(ts))
-        emd_fs.append(nbytes)
-        print(nbytes)
+            emd = EMD.EMD()
+            nbytes, f = emd.save(np.asarray(ts))
+            emd_fs.append(nbytes)
+            
 
     frame = next(file)
 
+except StopIteration:
+    print(sum(np.array(emd_fs) - np.array(flac_fs))/sum(flac_fs))
+    
 
-
-print(sum(np.array(emd_fs) - np.array(flac_fs)))
+print(sum(np.array(emd_fs) - np.array(flac_fs))/sum(flac_fs))
