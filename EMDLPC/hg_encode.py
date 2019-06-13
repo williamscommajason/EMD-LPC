486eccc3735ff73c3c4f34be88c95e7d60da14f4
Jason <williamscommajason@github.com>
2019-06-12 22:30:05 -0700

    almost working encoder for k = 0

--- EMDLPC/hg_encode.py
+++ EMDLPC/hg_encode.py
@@ -3,59 +3,110 @@ import math
 import numpy as np
 from io import BytesIO
 import rice_encode
+import scipy.io as io
+from rice_encode import signed_to_unsigned
 
-class Node:
+class Node(object):
+    pass
+
+class InternalNode(Node):
 
     def __init__(self,value):
         self.value = value
         self.left = None
         self.right = None
 
+class Leaf(Node):
+
+    def __init__(self,code):
+        self.code = str(code)
+
 class HyTree:
 
     def __init__(self, root):
 
         
-        self.root = Node(root)
+        self.root = InternalNode(root)
         self.value = self.root.value 
-
+        self.right = self.root.right
+        self.left = self.root.left
 
 def padding(bit):
 
     return '0' + bit
 
-def make_hybrid_tree(L,k):
+def rice_code(x,k):
 
-    f = BytesIO()
-    '''    
-    for j in L:
-        if j <= 2**k  - 1:
-            f = rice_encode.rice_code(f,j,k)
-        if j <= 2**(k+1) - 1 and j >= 2**k:
+    q = x / (1 << k)
+    q = int(q)
+    code = ''
+    for i in range(q):
+        code += '1'
+    code += '0'
 
-            f = rice_encode.rice_code(f,j,k)
-    '''
-    left_count = '1'
+    for i in range(k-1, -1, -1):
+        code += str((x >> i) & 1)
+
+    return code
+
+def make_hybrid_tree(level,k):
+
+    dictionary = dict()
+    
+    j = 0
+    
+    while j <= 2**(k+1) - 1:
+        code = rice_code(j,k)
+        dictionary[code] = j
+        j += 1
+    
+    left_count = ''
     tree = HyTree(left_count)
+    
     start = tree
-   
+    integer = j
+    max_reached = False
     i = 1
-    while i <= 4:
-        start.left = Node(start.value + '1')
-        start = make_leaf(start.left, 0)
+    while i <= level:
+        start = move(start,'1')
+        start,integer,dictionary = make_leaf(start, 0, integer,dictionary)
+        
         i += 1
 
-def make_leaf(start, k):
+    print(dictionary)
+    return tree , dictionary
+    
+    
+def move(node,bit,leaf = False):
+
+    if bit == '0' and leaf == False:
+        node.right = InternalNode(node.value + '0')        
+        return node.right
+
+    elif bit == '1' and leaf == False:
+        node.left = InternalNode(node.value + '1')
+        return node.left
+
+    elif bit == '0' and leaf == True:
+        node.right = Leaf(node.value + '0')
+        return node.right
+
+    else:
+        node.left = Leaf(node.value + '1')
+        return node.left
+
+
+def make_leaf(start, k, integer,dictionary):
             
     left_count = len(start.value)
+    
+
     i = left_count
-    start.right = Node('0')
     original = start
-    start = start.right
-    integer = 2**k
+    start = move(start,'0')
     even = 2*left_count + k
     odd = 2*left_count + k + 1
-
+    
     even_range = list(range(0,(2**k)*((2**i) -1) - 1))
     odd_range = list(range((2**(k+1))*((2**(i-1))-1),2**(i+k)+1))
     
@@ -70,10 +121,9 @@ def make_leaf(start, k):
             even_bits.remove(even_bit)
             continue
         even_pad = even - 1 - left_count
-        count = 1
-        while count < even_pad:
+
+        while len(even_bit) < even_pad:
             even_bit = padding(even_bit)
-            count += 1
 
         padded_even_bits.append(even_bit)
 
@@ -82,35 +132,133 @@ def make_leaf(start, k):
         if left_count + 1 + len(odd_bit) > odd:
             odd_bits.remove(odd_bit)
             continue
+        odd_pad = odd - 2 - left_count
 
-        odd_pad = odd - 1 - left_count
-        count = 1
-        while count < odd_pad:
+        while len(odd_bit) < odd_pad:
             odd_bit = padding(odd_bit)
-            count += 1
+           
         
         padded_odd_bits.append(odd_bit)
     
-    for even_bit in padded_even_bits:
-        for bit in even_bit:
-            if bit == '0':
-                start.right = Node('0')
-            else:
-                start.left = Node('1')
-        
-    for odd_bit in padded_odd_bits:
-        for bit in odd_bit:
-            if bit == '0':
-                start.right = Node('0')
-            else:
-                start.left = Node('1')
-    
-    return original
+    if left_count > 1:
 
+        padded_odd_bits, padded_even_bits = delete_prefixes(padded_odd_bits,padded_even_bits)
+
+    
+    for even_bit in padded_even_bits:
+        
+        start = original.right
+        for bit in even_bit[:-1]:
+            start = move(start,bit)
+        
+        start = move(start,even_bit[-1], leaf = True)
+        dictionary[str(integer)] = start.code
+        integer += 1
+        
+
+    for odd_bit in padded_odd_bits:
+        
+        start = original.right
+        for bit in odd_bit[:-1]:
+             start = move(start,bit)
+        
+        start = move(start,odd_bit[-1],leaf = True)
+        dictionary[str(integer)] = start.code
+        integer += 1
+        
+        
+    return original, integer, dictionary
+    
+    
+
+def delete_prefixes(padded_odd_bits,padded_even_bits):
+    
+    for even_bit in padded_even_bits:
+        for odd_bit in padded_odd_bits:
+            if even_bit == odd_bit[:-1]:
+                
+                padded_even_bits.remove(even_bit)
+                break
+    return padded_odd_bits, padded_even_bits
+
+def put_bit(f,b):
+    
+    global buff, filled
+     
+    buff = buff | (b << (7-filled))
+
+    if filled == 7:
+
+        f.write(buff.to_bytes(1,byteorder='little'))
+        buff = 0
+        filled = 0
+
+    else:
+        filled += 1
+
+
+def hybrid_encode(f, x, dictionary):
+
+    
+    code = dictionary[str(x)] 
+
+    for bit in code:
+        print(bit , code)
+        put_bit(f, int(bit))
+
+def pre_compress(L, level, k):
+    L = signed_to_unsigned(L)
+
+    fd = BytesIO()
+    tree, dictionary = make_hybrid_tree(level,k)
+
+    global buff,filled 
+    buff = 0
+    filled = 0
+ 
+    for x in L:
+        hybrid_encode(fd, x, dictionary)
+    
+    for i in range(8-filled):
+        put_bit(fd,1)
+
+    return fd.tell()
+
+def compress(f,L, level, k):
+
+    L = signed_to_unsigned(L)
+    size = pre_compress(L, level, k)
+    tree,dictionary = make_hybrid_tree(level,k)
+
+    #f.write(struct.pack('@i',k))
+    #f.write(struct.pack('@Q',size))
+
+    buff = 0
+    filled = 0
+
+    for x in L:
+
+        hybrid_encode(f, x, dictionary)
+
+    for i in range(8-filled):
+        put_bit(f, 1)
+
+    return f
+
+        
+        
 
 if __name__ == '__main__':
-
-    L = [1,2,3,4,5,5,6]
-
-    make_hybrid_tree(L,0)
-    
+    from EMDLPC import EMD
+    x = np.load('timestream1000.npy')[:100]
+    emd = EMD.EMD()
+    emd.save(x)
+    L = [2,3,4,5,5,6]
+    tree = make_hybrid_tree(4,0)
+    fp = BytesIO()
+    #fp = compress(fp,L,4,0)
+    print(fp.tell())
+    error = io.loadmat('error.mat')['error'][0]
+    error = [int(x) for x in error]
+    fp = compress(fp,emd.error,13,0)
+    print(fp.tell())
